368f1a0e7528c956ad57212e09ac6a15cfdf5f9a
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-10 13:01:14 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,22 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  let counter = document.getElementById("counter")
   let countPaused = false;
 
   //Will increase or decrease the counter
   function changeCounter(action){
+    let counter = document.getElementById("counter");
     let countInt = parseInt(counter.innerText, 10);
     if(action === 'increment') {
       countInt++;
     } else if (action === 'decrement') {
       countInt--;
     }
-    console.log(countInt)
     counter.innerText = countInt.toString(10);
   }
 
   function increaseEverySecond(){
-    window.setInterval(changeCounter('increment'), 1);
+    window.setInterval(changeCounter('increment'), 1000);
   }
 
   function paused(){
