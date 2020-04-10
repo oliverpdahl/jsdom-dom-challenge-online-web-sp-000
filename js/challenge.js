9f56faccff73e9e68150b45b84da25f68a5e6690
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-10 00:04:15 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -5,9 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
     let countInt = parseInt(counter.innerText, 10);
     if(action === 'increment') {
       countInt++;
+    } else if (action === 'decrement') {
+      countInt--;
     }
     console.log(countInt)
     counter.innerText = countInt.toString(10);
   }
-  changeCounter('increment')
+  changeCounter('decrement')
 });
