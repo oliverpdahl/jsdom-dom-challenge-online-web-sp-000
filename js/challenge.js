5ab2b60d3699a84d134e7a5fc1266599deeb2ee9
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-10 00:02:53 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let counter = document.getElementById("counter")
   function changeCounter(action){
+    let countInt = parseInt(counter.innerText, 10);
     if(action === 'increment') {
-      let countInt = parseInt(counter.innerText, 10);
       countInt++;
-      console.log(countInt)
-      counter.innerText = countInt.toString(10);
     }
+    console.log(countInt)
+    counter.innerText = countInt.toString(10);
   }
   changeCounter('increment')
 });
